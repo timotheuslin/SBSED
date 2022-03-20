e07c7676bb566d59b9e0015d2e0d16e00b2c9c98
Timothy Lin <Kuei-Hung.Lin@amd.com>
2022-03-19 23:15:05 -0700

    Update sbsed.py
    new: auto-extend, pylint fix

--- sbsed.py
+++ sbsed.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
+# -*- coding: utf-8 -*-
+# pylint: disable=invalid-name, line-too-long, too-many-nested-blocks, too-many-branches, too-many-locals
+# pylint: disable=logging-fstring-interpolation, redefined-outer-name
+# pylint: disable=broad-except, logging-not-lazy
+# pylint: disable=eval-used
+# pylint: disable=too-few-public-methods
+# pylint: disable=consider-using-with
+#
+
 """Simple Bytes Editor / Binary Stream Editor.
 
 Usage:
@@ -12,35 +21,8 @@ https://opensource.org/licenses/BSD-2-Clause
 
 Copyright (c) 2018, Timothy Lin
 All rights reserved.
+"""
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-The views and conclusions contained in the software and documentation are those
-of the authors and should not be interpreted as representing official policies,
-either expressed or implied, of the "Simple Binary Stream Editor" project."""
-
-
-from __future__ import print_function
-
-import os
 import re
 import sys
 import math
@@ -48,11 +30,19 @@ import shlex
 import argparse
 import traceback
 
+verbose_threshold = 1
 
-def ShlexSplit(shstr, splitters={','}, escape=''):
+def debug_message(msg, verbose=1):
+    """ print debug message according to the threshold setting """
+    if verbose > verbose_threshold:
+        print(msg)
+
+def ShlexSplit(shstr, splitters=None, escape=''):
     """Syntactically split a string with specified splitters, which honors its sub-strings
        in the quotation marks."""
 
+    if splitters is None:
+        splitters = {','}
     lex = shlex.shlex(shstr)
     lex.escape = escape
     lex.commenters = ''
@@ -65,9 +55,10 @@ def ShlexSplit(shstr, splitters={','}, escape=''):
             parms += ['']
     return parms
 
-    
+
 def LengthAdjust(bstr, width=0):
-    """Adjust the length of a byte-string to meet the byte-width with leading padding "0" regardless of its endianess.
+    """Adjust the length of a byte-string to meet the byte-width with leading padding "0"
+        regardless of its endianness.
 
     bstr - the byte string.\n
     width - the data length, counted in byte."""
@@ -92,7 +83,8 @@ def Bs2Ba(bstr, width=0):
 
 
 def Le2N(bstr, width=0):
-    """Conversion from the little-endian hex-digit byte-array to the network-byte-order hex-digit byte-array.
+    """Conversion from the little-endian hex-digit byte-array to the network-byte-order
+        hex-digit byte-array.
 
     bstr - the byte string.\n
     width - the data length, counted in byte."""
@@ -102,15 +94,15 @@ def Le2N(bstr, width=0):
 
 
 def LeInt(source, width=0):
-    """Conversion from a normal decimal-digit string to the network-byte-order hex-digit byte-array.
+    """Conversion from a normal decimal-digit string to the network-byte-order hex-digit
+        byte-array.
 
     bstr - the byte string.\n
     width - the data length, counted in byte."""
 
     if sys.byteorder == 'little':
         return Le2N(hex(int(source)), width)
-    else:
-        return Bs2Ba(hex(int(source)))
+    return Bs2Ba(hex(int(source)))
 
 
 def Guid2N(gstr):
@@ -118,29 +110,28 @@ def Guid2N(gstr):
 
     # The canonical 8-4-4-4-12 format GUID string:
     # 123e4567-e89b-12d3-a456-426655440000
-    # xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx 
+    # xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx
     gs = gstr.split('-')
     try:
-        if len(gs) != 5: raise Exception('')
+        if len(gs) != 5:
+            raise Exception('')
         return Le2N(gs[0], 4) + Le2N(gs[1], 2) + Le2N(gs[2], 2) + Bs2Ba(gs[3], 2) + Bs2Ba(gs[4], 6)
     except Exception:
-        raise Exception('Incorrect Guid format:%s' % gstr)
+        print(f'Error: Incorrect GUID format: {gstr}')
+        raise
 
 
-def SafeEval(evalstr, default=None, globalz=None, localz=None):
-    """A safer wrapper for eval()."""
+def ParseInteger(evstr):
+    """Parse a integer"""
 
-    if not globalz:
-        globalz = {}
-    if not localz:
-        localz = {}
-    try:
-        return eval(evalstr, globalz, localz)
-    except SyntaxError:
-        return default if default else evalstr
+    if evstr in {'+'}:
+        return evstr
+    #try:
+    return int(eval(evstr, {}, {}))
+    #except (SyntaxError, NameError) as ex:
+    #    raise
 
-
-class EditorAction(object):
+class EditorAction():
     """The editor's action."""
     def __init__(self, edstr):
         self.target_offset = None
@@ -151,10 +142,11 @@ class EditorAction(object):
         self.from_offset = None
         self.encoding = 'utf-8'
         eds = ShlexSplit(edstr, {':'})
+        debug_message(f'{str(eds)}')
         try:
-            self.target_offset = SafeEval(eds[0]) if eds[0] else None
+            self.target_offset = ParseInteger(eds[0])
             self.source_data = eds[1]
-            self.length = SafeEval(eds[2]) if eds[2] else None
+            self.length = ParseInteger(eds[2])
             self.operation = eds[3].lower()
         except IndexError:
             pass
@@ -171,17 +163,16 @@ class EditorAction(object):
         def intx(data, width):
             if data_data.startswith('0x'):
                 return Le2N(data[2:], width)
-            else:
-                return LeInt(data, width)
+            return LeInt(data, width)
 
         if len(data_x) == 1:
             self.hexdata = strx(self.source_data)
         elif len(data_x) == 2:
             data_type, data_data = data_x
             if not data_type or not data_data:
-                raise Exception('Incorrect source data: [%s]' % self.source_data)
+                raise Exception(f'Incorrect source data: [{self.source_data}]')
 
-            intz = re.match('(i|int|integer)(\d+)', data_type)
+            intz = re.match(r'(i|int|integer)(\d+)', data_type)
             if intz:
                 # Handle these integers: i8, int8, intger8, i16, int16, integer 16....
                 # Note: integer9 would be treated as integer16
@@ -200,7 +191,7 @@ class EditorAction(object):
                 self.hexdata = strx(data_data)
                 self.from_offset = int(data_data, base=0)
         if not self.hexdata:
-            raise Exception('Invalid data type or format:%s.' % self.source_data)
+            raise Exception(f'Invalid data type or format: {self.source_data}')
 
         if isinstance(self.length, int):
             remain_len = self.length - len(self.hexdata)
@@ -214,33 +205,43 @@ class EditorAction(object):
 
     def __repr__(self):
         def _repr(member):
-            if (member is None) or (not isinstance(member, int)):
-                return '%s' % str(member)
-            else:
-                return '0x%X(%d)' % (member, member)
+            if member in {None, '+'}:
+                return 'auto'
+            if isinstance(member, int):
+                if member < 10:
+                    return f'{member}'
+                return f'{member:d}(0x{member:X})'
+            return str(member)
         return '\n'.join([
-            '  target offset    : %s' % _repr(self.target_offset),
-            '  source data      : %s' % str(self.source_data),
-            '  length           : %s' % _repr(self.length),
-            '  operation        : %s' % self.operation,
+            f'  target offset    : {_repr(self.target_offset)}',
+            f'  source data      : {str(self.source_data)}',
+            f'  length           : {_repr(self.length)}',
+            f'  operation        : {self.operation}',
         ])
 
 
 class CommandArgument(argparse.ArgumentParser):
+    """The editor's argument"""
     def __init__(self, usage=''):
         if not usage:
             usage = 'bsed --file source[,target] --edit editor_action [editor_action_1 [editor_action_2]'
         argparse.ArgumentParser.__init__(self, usage=usage, prefix_chars='-/', fromfile_prefix_chars='@', add_help=False)
         self.add_argument('-f', '--file', dest='file', metavar='File', nargs='+', help='Specify the source file and the optional target file.')
         self.add_argument('-e', '--edit', dest='edit', metavar='Edit', nargs='+', help='The editor action that consists of offset:data[:length:[operation]]')
+        self.add_argument('-x', '--auto_extend', action='store_true', help="Automatically extend the file size when writing data over the file boundary.")
+        #self.add_argument('-d', '--dry-run', action='store_true', help="Dry run.")
         args, __unknown = self.parse_known_args(sys.argv[1:])
 
+        if __unknown:
+            self.specific_help = f'Surplus arguments: {__unknown}'
+
         self.edits = []
+        self.auto_extend = args.auto_extend
         self.specific_help = ''
         self.need_help = self.format_help()
         if not args.file:
             self.specific_help = 'No specified file.'
-            return 
+            return
         if not args.edit:
             self.specific_help = 'No specified editor action.'
             return
@@ -256,75 +257,97 @@ class CommandArgument(argparse.ArgumentParser):
             self.output_file = ''
 
         for ed in args.edit:
+            debug_message(f'ed: {str(ed)}')
             try:
                 self.edits += [EditorAction(ed)]
             except Exception:
-                self.specific_help = 'Invalid editor action:%s' % ed
+                self.specific_help = f'Invalid editor action: {ed}'
                 raise #Exception('')
         self.need_help = ''
-        
+
     def help(self):
-        if arg.specific_help:
-            print('Argument error: %s\n' % arg.specific_help)
-        if arg.need_help:
-            print('%s' % arg.need_help)
+        """ print  help message """
+        print('Binary Steam Editor version 1.0\n')
+        if self.specific_help:
+            print(f'Argument error: {self.specific_help}\n' )
+        if self.need_help:
+            print(self.need_help)
         sys.exit(1)
 
     def __repr__(self):
         strs = []
-        strs += ['Source File: %s' % self.input_file]
-        strs += ['Target File: %s' % self.output_file]
-        strs += ['Editor Action%s:' % ('s' if len(self.edits)>1 else '')]
+        strs += [f'Source File: {self.input_file}']
+        strs += [f'Target File: {self.output_file if self.output_file else self.input_file}']
+        strs += [f'Editor Action{"s" if len(self.edits)>1 else ""}:']
         for ed in self.edits:
             strs += [str(ed), '']
         return '\n'.join(strs)
 
 
-class Editor(object):
+class Editor():
     """A UTF-8 editor"""
-    
-    def __init__(self, input_file, output_file=None):
+
+    def __init__(self, input_file, output_file=None, auto_extend=False):
         self.input_file = input_file
         self.output_file = output_file if output_file else input_file
-        with open(input_file, 'rb') as fin:
-            self.content = bytearray(fin.read())
-        self.changed = False
-        self.PreviousAction = None
+        self.auto_extend = auto_extend
+        self.content = bytearray()
+        self.changed = 0
+        self.previous_action = None
+        try:
+            self.content = bytearray(open(input_file, 'rb').read())
+        except FileNotFoundError:
+            if not self.auto_extend:
+                raise
 
     def overwrite(self, action):
+        """ overwrite a block of content """
+        debug_message(f'action.target_offset: {action.target_offset}')
+        debug_message(f'action.from_offset:   {action.from_offset}')
+        debug_message(f'action.length:        {action.length}')
+        debug_message(f'action.source_data:   {str(action.source_data)}')
+
         if action.from_offset is not None:
             data = self.content[action.from_offset:action.from_offset+action.length]
             dlen = len(data)
         #elif action.from_file is not None:
         #    pass
         else:
+            debug_message(f'auto_extend: {self.auto_extend}')
+            lenx = action.target_offset + action.length - len(self.content)
+            if lenx > 0 and self.auto_extend:
+                self.content.extend(bytearray(lenx))
             dlen = min(action.length, len(self.content) - action.target_offset)
+            if dlen < 1:
+                return # BUGBUG
             data = action.hexdata[:dlen]
-        if dlen < 1:
-            return # Ignore the boundary-overrun error. Or, what else shall we do?
-        # BUGBUG. verify this: boundary-hit.
-        self.content[action.target_offset:action.target_offset+dlen] = data
-        self.changed = True
+        if self.content[action.target_offset:action.target_offset+dlen] != data:
+            self.content[action.target_offset:action.target_offset+dlen] = data
+            self.changed += 1
 
     def edit(self, action):
-        if action.target_offset in {None, '+'} and self.PreviousAction:
-            action.target_offset = self.PreviousAction.target_offset
+        """ apply the editor's action """
+        if action.target_offset in {None, '+'} and self.previous_action:
+            action.target_offset = self.previous_action.target_offset
             if action.target_offset in {None, '+'}:
                 raise Exception('Invalid target offset')
-            action.target_offset += self.PreviousAction.length
+            action.target_offset += self.previous_action.length
 
         if action.operation in {'overwrite', 'copy'}:
             self.overwrite(action)
         else:
-            print('Unsupported editor action: %s' % action.operation)
-        self.PreviousAction = action
+            print(f'Unsupported editor action: {action.operation}')
+        self.previous_action = action
 
     def commit(self):
         """Apply the changes to the target file."""
 
-        if self.changed:
-            with open(self.output_file, 'wb') as fout:
-                fout.write(self.content)
+        if not self.changed:
+            print('No modification.')
+            return
+        with open(self.output_file, 'wb') as fout:
+            fout.write(self.content)
+        print(f'{self.changed} change{"s" if self.changed>1 else ""} updated.')
 
 if __name__ == '__main__':
     arg = CommandArgument()
@@ -335,13 +358,15 @@ if __name__ == '__main__':
     input_file = arg.input_file
     output_file = arg.output_file if arg.output_file else arg.input_file
     try:
-        ed = Editor(input_file, output_file)
+        ed = Editor(input_file, output_file, auto_extend=arg.auto_extend)
     except IOError:
         traceback.print_exc()
         sys.exit(2)
 
+    print(str(arg))
+
     for ar in arg.edits:
         ed.edit(ar)
     ed.commit()
-        
-    print('%s' % str(arg))
+
+    sys.exit(0)
